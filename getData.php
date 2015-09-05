078a3b9ec102d0b6031c0764e52350653c4063fd
isafo <isabelle.forsman@gmail.com>
2015-09-05 10:54:33 +0200

    following example

--- getData.php
+++ getData.php
@@ -2,41 +2,15 @@
 <html>
   <body>
     <?php
-      $servername = "localhost";
-      $username = "usr";
-      $password = "pass";
-      $dbname = "itsnoworry";
 
-      // create connection
-      $conn = new mysqli($servername, $username, $password, $dbname);
-
-      //check connction
-      if($conn->connection_error)
-        die("Connection failed:" . $conn->connection_error);
-
-      $sql = "SELECT * FROM data"
-      $result = $conn->query($sql);
-
-      if($result->num_rows > 0) {
-        // output data for each row
-        while($row = $result->fetch_assoc()) {
-          echo "pressure: " . $row["pressure"];
-        }
-
-      } else {
-        echo "no results found!";
-      }
-
-      $conn->close();
-
-
-
-
-
+      $result;
       echo json_encode(42);
     ?>
 
-
+    <?php
+      $cars = array("Volvo", "BMW", "Toyota");
+      echo "I like " . $cars[0] . ", " . $cars[1] . " and " . $cars[2] . ".";
+    ?>
 
 
   </html>
