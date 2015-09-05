04e50384240d79d39bb87d0f6f93048ce1d3475d
isafo <isabelle.forsman@gmail.com>
2015-09-05 11:15:16 +0200

    ..

--- getData.php
+++ getData.php
@@ -1,17 +1,15 @@
 <!DOCTYPE html>
 <html>
   <body>
-    <?php
 
-      $result;
+    <?php
+      echo "My first PHP script!";
+
       echo json_encode(42);
-    ?>
 
-    <?php
       $cars = array("Volvo", "BMW", "Toyota");
       echo "I like " . $cars[0] . ", " . $cars[1] . " and " . $cars[2] . ".";
     ?>
-
 
   </html>
 </body>
