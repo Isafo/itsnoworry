2ea1aa138057ee591014ffc9e38887e1c8442a4f
isafo <isabelle.forsman@gmail.com>
2015-09-05 14:19:12 +0200

    hopefully fixed the stuff

--- getData.php
+++ getData.php
@@ -3,6 +3,15 @@
   <body>
 
     <?php
+
+    /* Do some operation here, like talk to the database, the file-session
+    * The world beyond, limbo, the city of shimmers, and Canada.
+    *
+    * AJAX generally uses strings, but you can output JSON, HTML and XML as well.
+    * It all depends on the Content-type header that you send with your AJAX
+    * request. */
+
+
       echo "My first PHP script!";
 
       echo json_encode(42);
---
+++ temp.php
@@ -0,0 +1,14 @@
+<script>
+function() reqListener() {
+  console.log(this.responseText);
+}
+
+var oReq = new XMLHttpRequest(); // create request object
+oReq.onload = function() {
+  // handle the data
+  var pressureData[] = this.responseText();
+}
+
+oReq.open("get", "getData.php", true);s
+oReq.send();
+</script>
